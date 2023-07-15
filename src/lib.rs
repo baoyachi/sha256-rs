d69ba7471b7922b27377a4678ec993375849a806
baoyachi <liaoymxsdl@gmail.com>
2023-07-15 13:26:08 +0800

    refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,7 @@
 //!
 //! ```
 
+use sha2::digest::Output;
 use sha2::{Digest, Sha256};
 use std::fmt::Debug;
 use std::fs;
@@ -151,24 +152,100 @@ impl Sha256Digest for &String {
 impl TrySha256Digest for &Path {
     type Error = io::Error;
     fn digest(self) -> Result<String, Self::Error> {
-        let mut sha = Sha256::new();
         let f = fs::File::open(self)?;
-        let mut reader = BufReader::new(f);
-        // let bytes = fs::read(self)?;
-        let mut buf = [0u8; 1024];
-        loop {
-            let len = reader.read(&mut buf)?;
-            if len == 0 {
-                break;
-            }
-            sha.update(&buf[0..len]);
-        }
-        let hash = sha.finalize();
-
-        Ok(hex::encode(hash))
+        let reader = BufReader::new(f);
+        let sha = Sha256::new();
+        Ok((reader, sha).calc())
     }
 }
 
 fn __digest__(data: &[u8]) -> String {
-    hex::encode(sha2::Sha256::digest(data))
+    hex::encode(Sha256::digest(data))
+}
+
+trait Calculator {
+    type FinishType: AsRef<[u8]>;
+    fn calc(mut self) -> String
+    where
+        Self: Sized,
+    {
+        let mut buf = [0u8; 1024];
+        loop {
+            let len = self.read(&mut buf).unwrap();
+            if len == 0 {
+                break;
+            }
+            self.update(&buf[0..len]);
+        }
+        let hash = self.finish();
+        hex::encode(hash)
+    }
+
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize>;
+    fn update(&mut self, data: &[u8]);
+    fn finish(self) -> Self::FinishType;
+}
+
+impl<R> Calculator for (R, Sha256)
+where
+    R: Read,
+{
+    type FinishType = Output<Sha256>;
+
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.0.read(buf)
+    }
+
+    fn update(&mut self, data: &[u8]) {
+        self.1.update(data)
+    }
+
+    fn finish(self) -> Self::FinishType {
+        self.1.finalize()
+    }
+}
+
+#[cfg(feature = "native_openssl")]
+mod openssl_sha256 {
+    use crate::Calculator;
+    use std::io::Read;
+
+    type OpenSslSha256 = openssl::sha::Sha256;
+
+    impl<R> Calculator for (R, OpenSslSha256)
+    where
+        R: Read,
+    {
+        type FinishType = [u8; 32];
+
+        fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+            self.0.read(buf)
+        }
+
+        fn update(&mut self, data: &[u8]) {
+            self.1.update(data)
+        }
+
+        fn finish(self) -> Self::FinishType {
+            self.1.finish()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(feature = "native_openssl")]
+    #[test]
+    fn test_openssl_sha256() {
+        let input = Path::new("./foo.file");
+        let f = fs::File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        let sha = openssl::sha::Sha256::new();
+        assert_eq!(
+            "433855b7d2b96c23a6f60e70c655eb4305e8806b682a9596a200642f947259b1",
+            (reader, sha).calc()
+        );
+    }
 }
